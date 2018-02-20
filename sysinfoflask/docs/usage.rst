b77d49410fe3a1a50e74eba8524483ccbe274ebc
Hugh <hug_hob@hotmail.com>
2018-02-20 09:18:52 +0000

    2nd commit

--- sysinfoflask/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use sysinfoflask in a project::
-
-    import sysinfoflask
--- sysinfoflask/sysinfoflask/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for sysinfoflask."""
-
-__author__ = """Hugh O'Brien"""
-__email__ = 'hugh.obrien@ucdconnect.ie'
-__version__ = '0.1.0'
